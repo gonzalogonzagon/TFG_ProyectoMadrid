75aa93acf02e74c00480f5983e4fd7e81b012566
Gonza Rico <gonzaricoesteban@gmail.com>
2024-12-28 22:58:24 +0100

    Bottom bar nav menu

--- app/src/main/java/com/example/tfg_proyectomadrid/MainActivity.kt
+++ app/src/main/java/com/example/tfg_proyectomadrid/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.tfg_proyectomadrid
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tfg_proyectomadrid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +30,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initUI()
+    }
+
+    // (Made by user) Function to initialize the UI and call other functions
+    private fun initUI() {
+        initNavigation()
+    }
+
+    // (Made by user) Function to initialize the navigation
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main) as? NavHostFragment
+        if (navHostFragment != null) {
+            navController = navHostFragment.navController
+            binding.bottomNavMenu.setupWithNavController(navController)
+        } else {
+            // Manejo del caso en que el fragmento no se encuentra
+            handleError("NavHostFragment no encontrado")
+        }
+    }
+
+    // (Made by user) Function to handle errors
+    private fun handleError(message: String) {
+        // Mostrar mensaje al usuario
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        // Registrar el error en la consola
+        Log.e("MainActivity", message)
     }
 }
