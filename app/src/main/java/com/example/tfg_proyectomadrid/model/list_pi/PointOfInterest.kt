c85fa7731b93c9743ee690b30d633a206422f600
Gonza Rico <gonzaricoesteban@gmail.com>
2025-01-20 17:51:07 +0100

    Data model and provider modifications

--- app/src/main/java/com/example/tfg_proyectomadrid/model/list_pi/PointOfInterest.kt
+++ app/src/main/java/com/example/tfg_proyectomadrid/model/list_pi/PointOfInterest.kt
@@ -1,47 +1,12 @@
 package com.example.tfg_proyectomadrid.model.list_pi
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import com.example.tfg_proyectomadrid.R
 
-sealed class PointOfInterest(
+data class PointOfInterest(
     @StringRes val title: Int,
     @StringRes val description: Int,
+    @ColorRes val category: Int,
     @DrawableRes val image: Int
-) {
-    data object PlazaMayor : PointOfInterest(
-        title = R.string.plaza_mayor_title,
-        description = R.string.plaza_mayor_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-
-    data object PuertaDelSol : PointOfInterest(
-        title = R.string.puerta_sol_title,
-        description = R.string.puerta_sol_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-
-    data object ParqueDelRetiro : PointOfInterest(
-        title = R.string.parque_retiro_title,
-        description = R.string.parque_retiro_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-
-    data object PalacioReal : PointOfInterest(
-        title = R.string.palacio_real_title,
-        description = R.string.palacio_real_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-
-    data object GranVia : PointOfInterest(
-        title = R.string.gran_via_title,
-        description = R.string.gran_via_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-
-    data object TemploDeDebod : PointOfInterest(
-        title = R.string.templo_debod_title,
-        description = R.string.templo_debod_description,
-        image = R.drawable.ic_launcher_foreground
-    )
-}
+)
--- app/src/main/java/com/example/tfg_proyectomadrid/model/list_pi/PointOfInterestProvider.kt
+++ app/src/main/java/com/example/tfg_proyectomadrid/model/list_pi/PointOfInterestProvider.kt
@@ -1,16 +1,49 @@
 package com.example.tfg_proyectomadrid.model.list_pi
 
-import com.example.tfg_proyectomadrid.model.list_pi.PointOfInterest.*
+import com.example.tfg_proyectomadrid.R
 
-class PointOfInterestProvider {
-    companion object {
-        val pointOfInterestList : List<PointOfInterest> = listOf(
-            PlazaMayor,
-            PuertaDelSol,
-            ParqueDelRetiro,
-            PalacioReal,
-            GranVia,
-            TemploDeDebod
+object PointOfInterestProvider {
+    val pointOfInterestList: List<PointOfInterest> = listOf(
+        PointOfInterest(
+            title = R.string.plaza_mayor_title,
+            description = R.string.plaza_mayor_description,
+            category = R.color.yellow,
+            image = R.drawable.ic_launcher_foreground
+        ),
+
+        PointOfInterest(
+            title = R.string.puerta_sol_title,
+            description = R.string.puerta_sol_description,
+            category = R.color.blue,
+            image = R.drawable.ic_launcher_foreground
+        ),
+
+        PointOfInterest(
+            title = R.string.parque_retiro_title,
+            description = R.string.parque_retiro_description,
+            category = R.color.gray,
+            image = R.drawable.ic_launcher_foreground
+        ),
+
+        PointOfInterest(
+            title = R.string.palacio_real_title,
+            description = R.string.palacio_real_description,
+            category = R.color.green,
+            image = R.drawable.ic_launcher_foreground
+        ),
+
+        PointOfInterest(
+            title = R.string.gran_via_title,
+            description = R.string.gran_via_description,
+            category = R.color.cyan,
+            image = R.drawable.ic_launcher_foreground
+        ),
+
+        PointOfInterest(
+            title = R.string.templo_debod_title,
+            description = R.string.templo_debod_description,
+            category = R.color.magenta,
+            image = R.drawable.ic_launcher_foreground
         )
-    }
+    )
 }
